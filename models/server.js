5e930f0d5169d36d414046c15ac3004536809a75
josemoralesmc <josemorales270102@gmail.com>
2023-02-20 12:08:10 -0300

    Backend Basico

---
+++ models/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+class Server {
+
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT
+        this.usuariosPath = '/api/usuarios'
+
+        // Middlewares
+
+        this.middlewares()
+
+        // parseo y lectura de body
+
+        this.app.use(express.json())
+
+        // Rutas de mi aplicacion
+        this.routes()
+    }
+
+    middlewares(){
+        // Directorio publico
+        this.app.use(cors()) 
+        this.app.use(express.static('public'))
+    }
+
+    routes(){
+        
+        this.app.use(this.usuariosPath, require('./routes/user.routes'))
+        
+    }
+
+    listen() {
+        this.app.listen(this.port, ( )=> {
+            console.log("Servidor corriendo en el puerto", this.port)
+        })
+    }
+
+}
+
+module.exports = Server
